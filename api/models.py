8af319613ffb7f98031d729419dd61e8119f6658
Keerthan Chand <chandkeerthan@gmail.com>
2020-11-20 11:43:42 +0530

    secondary changes

--- api/models.py
+++ api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+import string, random
 
-# Create your models here.
+def generate_unique_code():
+    length=6
+    while True:
+        code = "".join(random.choices(string.ascii_uppercase, k=length))
+        if Room.objects.filter(code=code).count() == 0:
+            break
+    return code
+
+class Room(models.Model):
+    code = models.CharField(default="", max_length=50, unique=True)
+    host = models.CharField(max_length=50, unique=True)
+    guest_can_pause = models.BooleanField(default=False, null=False)
+    votes_to_skip = models.IntegerField(null=False, default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
